9b39b0931594823230c6e6f8f0f163be0266af77
Alek <alek2750@gmail.com>
2018-03-08 10:40:30 +0100

    Servlet class

---
+++ Cupcake_web/src/main/java/Servlets/Servlet.java
@@ -0,0 +1,98 @@
+package Servlets;
+
+import Logic.Price;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Alek
+ */
+
+@WebServlet(name="webServlet", urlPatterns = {"/webServlet"})
+public class Servlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        Price price = new Price();
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet webServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            System.out.println("#####" + request.getParameter("attr1"));
+            System.out.println("#####" + request.getParameter("attr2"));
+            System.out.println("#####" + request.getParameter("choice"));
+            Double attr1 = Double.parseDouble(request.getParameter("attr1"));
+            Double attr2 = Double.parseDouble(request.getParameter("attr2"));
+            String choiceName = request.getParameter("choice");
+//            if (!valid.valid(attr1, attr2)) {
+//                response.sendRedirect("error.html");
+//            }
+            
+//            out.println("<h1>Servlet webServlet at " + price.calcPrice(choiceName,attr1, attr2) + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
